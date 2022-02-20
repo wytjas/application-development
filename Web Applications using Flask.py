76d5e2ecbcc2ff7f80e9ca377583ea96abfc810b
Jas Wang <75501427+wytjas@users.noreply.github.com>
2022-02-19 23:48:05 -0800

    Add files via upload

---
+++ Web Applications using Flask.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import json
+
+app = Flask("Book Manager")
+books = []
+
+@app.route("/addBook/<bookname>/<author>")
+def addBook(bookname,author):
+    books.append({"bookname":bookname,"author":author})
+    return "%s by %s appended" %(bookname,author)
+
+@app.route("/getBooks")
+def getBooks():
+    return json.dumps(books)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+if __name__=="__main__":
+    app.run(debug=True) 
